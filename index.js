b9576b8bef712e2e75f271ec87865ba09d954936
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-08-16 18:17:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require('./config.json');
 const Commando = require('discord.js-commando');
-const bot = new Commando.Client();
+const client = new Commando.Client();
 client.config = config;
 
 client.on("ready", () => {
