39a0aade6c05629a853ee46e8547ac4d74eafbbb
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-08-17 17:14:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -266,6 +266,8 @@ client.on("message", async message => {
   
   if(command === "purge") {
     // This command removes all messages from all users in the channel, up to 100.
+    if(!message.member.roles.some(r=>["Administrator", "Moderator", "Admin", "Mod", "Owner", "Co-Owner"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
     
     // get the delete count, as an actual number.
     const deleteCount = parseInt(args[0], 10);
