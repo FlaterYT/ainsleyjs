36d379b182747181885a65700e8cab3757fc79c3
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-08-17 18:33:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setActivity(`Rubbing ${client.guilds.size} servers`);
+  client.user.setActivity(`ainsley help`);
 });
 
 client.on("guildCreate", guild => {
