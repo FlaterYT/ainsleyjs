9496a58775f78f263d3dd6ad04ed81875a4c655c
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-08-17 06:36:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require('./config.json');
-const args = message.content.slice(prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
 client.config = config;
 
 client.on("ready", () => {
