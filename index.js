af725feaca77e2cb47bf0752180222bcac0620a2
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-08-17 21:51:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setActivity(`ainsley help for ${client.guilds.size} servers and ${client.users.size} members`);
+  client.user.setActivity(`ainsley help for ${client.users.size} members in ${client.guilds.size} servers.`);
+});
+
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`ainsley help for ${client.users.size} members in ${client.guilds.size} servers.`);
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`ainsley help for ${client.users.size} members in ${client.guilds.size} servers.`);
 });
 
 client.on('message', (message) => {
