e7bfae0724974e44f9f95caca1a29ff8be3c805d
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-08-17 23:43:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -158,8 +158,7 @@ client.on('message', (message) => {
 });
 
 client.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find('name', 'welcome');
-    let channel = member.guild.channels.find('name', 'welcome-leave');
+    let channel = member.guild.channels.find('name', 'welcome_leave');
     let memberavatar = member.user.avatarURL
         if (!channel) return;
         let embed = new Discord.RichEmbed()
