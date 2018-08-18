3e7f19c26e13df37b1af97c98f0ce78b9a163980
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-08-18 03:49:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client({ fetchAllMembers: true, sync: true });
 const config = require('./config.json');
+const fs = require('fs');
 const YTDL = require("ytdl-core");
 client.config = config;
 
