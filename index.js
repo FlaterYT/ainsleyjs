c7043af367358b0571750b25b5588dc8ee7b7993
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-08-18 06:07:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -308,6 +308,34 @@ client.on("message", async message => {
   }
 });
 
+client.on("message", function(message){
+	if (message.content === "8ball") {
+    	var sayings = ["It is certain",
+										"It is most certainly so",
+										"Without a doubt",
+										"Yes, definitely",
+										"You may rely on it",
+										"As I see it, yes",
+										"Most likely",
+										"Outlook good",
+										"Yes",
+										"Penis points to yes",
+										"Reply hazy try again",
+										"Ask again later",
+										"Better not tell you now",
+										"Cannot predict now",
+										"Concentrate and ask again",
+										"Don't count on it",
+										"My reply is no",
+										"My sources say no",
+										"Outlook not so good",
+										"Very doubtful"];
+
+			var result = Math.floor((Math.random() * sayings.length) + 0);
+			client.reply(message, sayings[result]);
+  }
+});
+
 client.on('message', (message) => {
     if(message.content == 'ainsley help'){
         message.channel.send({embed: {
