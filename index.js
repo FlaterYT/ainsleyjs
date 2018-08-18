9c000abb23cc1b8280b4cca1900f6f46cb401c06
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-08-18 06:20:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client({ fetchAllMembers: true, sync: true });
 const config = require('./config.json');
 client.config = config;
-const Limiter = require("./limit.js");
-let limit = new Limiter(15000);
-const fs = require("fs");
-
-const clockEmoji = ["🕐", "🕑", "🕒", "🕓", "🕔", "🕕", "🕖", "🕗", "🕘", "🕙", "🕚", "🕛", "🕜", "🕝", "🕞", "🕟", "🕠", "🕡", "🕢", "🕣", "🕤", "🕥", "🕦", "🕧"];
-
-const re = /^(\>\s?r+o+ll+|\>\s?s+h+a+k+e+)(?:\s(.+))?/gim; // Regex to find out if a user types ">roll"
-let responses;
-fs.readFile('./responses.txt', "utf8", function(err, data) { // Allows custom responses
-	if(err) {
-		console.log("Can't read or find responses.txt, setting defaults");
-		responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely",
-					"You may rely on it", "As I see it, yes", "Most Likely", "Outlook good", "Yes",
-					"Signs point to yes", "Reply hazy try again", "Ask again later"];
-	} else {
-		responses = data.split("\n"); // Responses are each individual line
-	}
-});
 
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
@@ -324,31 +306,6 @@ client.on("message", async message => {
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
-});
-
-client.on('ready', () => {
-  	console.log(`Logged in as ${client.user.tag}!`);
-	console.log("Available responses: " + responses.length);
-});
-
-client.on('message', msg => {
-	let matches = re.exec(msg.content);
-
-  	if (!msg.author.bot && matches) {
-  		if(limit && limit.exists(msg.author.id)) { // Checks if a usage limit is in place
-  			for(let i = 0; i < Math.floor(Math.random()*5); i++) {
-				msg.react(clockEmoji[Math.floor(Math.random()*clockEmoji.length)]);
-  			}
-		} else {
-			var rand = Math.floor(Math.random()*responses.length);
-	  		if(matches[2]) {
-	  			msg.reply("You asked: \"" + matches[2] + "\" - " + responses[rand]);
-	  		} else {
-	  			msg.reply(responses[rand]);
-	  		}
-	  		limit.add(msg.author.id);
-		}
-  	}
 });
 
 client.on('message', (message) => {
